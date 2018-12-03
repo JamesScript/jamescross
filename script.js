4f0260843208253e85e41b19cc6efb723ac814f5
crossjames934 <crossjames934@gmail.com>
2018-12-03 22:56:15 +0000

    bulked out menu and seperated stylesheets

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function menu(show) {
     if (show) {
         $("#hamburger").toggleClass("spinHide");
+        $("#closeHamburger").toggleClass("spinShow");
         $("#darkenScreen").toggleClass("darken");
+        $("#mainMenu")
+            .toggleClass("slideOn")
+            .css({pointerEvents: "auto"});
     } else {
 
     }
