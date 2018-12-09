297862d82611cfe181223838dacce72ba7f50423
crossjames934 <crossjames934@gmail.com>
2018-12-09 18:19:47 +0000

    intro panel with animated fly on based on offset height

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-function menu(show) {
-    if (show) {
-        $("#hamburger").toggleClass("spinHide");
-        $("#closeHamburger").toggleClass("spinShow");
-        $("#darkenScreen").toggleClass("darken");
-        $("#mainMenu")
-            .toggleClass("slideOn")
-            .css({pointerEvents: "auto"});
-    } else {
+let scrollHeight = 0;
+let topSectionHeight = $("#firstImpression").height();
 
+$(window).scroll(function () {
+    scrollHeight = $(window).scrollTop();
+    introFlyOn();
+});
+
+window.onresize = function() {
+    topSectionHeight = $("#firstImpression").height();
+    reshapePanel();
+};
+
+reshapePanel();
+
+function menu() {
+    $("#hamburger").toggleClass("spinHide");
+    $("#closeHamburger").toggleClass("spinShow");
+    $("#darkenScreen").toggleClass("darken");
+    $("#mainMenu")
+        .toggleClass("slideOn")
+        .css({pointerEvents: "auto"});
+}
+
+function introFlyOn() {
+    let distance = topSectionHeight - scrollHeight;
+    let fraction = 1 - distance / topSectionHeight;
+    console.log(fraction);
+    if (fraction <= 1) {
+        $("#introPanel")
+            .css("transform", `perspective(1000px) rotateY(${360 - fraction * 360}deg) scale(${fraction * 0.5})`)
+            .css("left", `${-100 + fraction * 110}vw`);
+    } else {
+        $("#introPanel")
+            .css("transform", `perspective(1000px) rotateY(0deg) scale(${fraction / 2})`)
+            .css("left", `10vw`);
+    }
+    // Turn hamburger menu white
+    if (fraction > 0.9) {
+        $(".menu").css("color", "white");
+    } else {
+        $(".menu").css("color", "black");
+    }
+}
+
+function reshapePanel() {
+    if (window.innerWidth < window.innerHeight) {
+        $(".leftSide")
+            .css("width", "100%")
+            .css("font-size", "3vh");
+        $(".rightSide").hide();
+    } else {
+        $(".leftSide")
+            .css("width", "calc(50% - 5vw)")
+            .css("font-size", "1.6vw");
+        $(".rightSide").show();
     }
 }
